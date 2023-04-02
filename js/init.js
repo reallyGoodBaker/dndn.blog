76c1a397196f55b6fb95568dcaf8256af5aff2be
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2023-04-02 18:30:11 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,4 @@
 import { bindAllCGs } from './cg.js'
-import { LoadingCircle } from './LoadingCircle.js'
-
-customElements.define('loading-circle', LoadingCircle)
 
 window.addEventListener('load', async () => {
     bindAllCGs()
